3d5d67d2d6596d6c9e29fdad2b774c8037a1700e
multiverse2011 <multiverse2011@gmail.com>
2020-07-23 21:16:50 +0900

    Initial commit

---
+++ app/src/main/java/com/ayushio/urlsummarizer/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.ayushio.urlsummarizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.RealmResults
+import io.realm.Sort
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val realm: Realm by lazy {
+        Realm.getDefaultInstance()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val taskList = readAll()
+
+        val adapter =
+            TaskAdapter(this, taskList, object : TaskAdapter.OnItemClickListener{
+            override fun onItemClick(item: Task) {
+                Toast.makeText(applicationContext, item.content + " deleted!", Toast.LENGTH_SHORT).show()
+                delete(item.id)
+            }
+        },true)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    private fun createDummyData() {
+        for (i in 0..10){
+            create("やること")
+        }
+    }
+
+    private fun update(id: String, content: String){
+        realm.executeTransaction {
+            val task = realm.where(Task::class.java).equalTo("id",id).findFirst()
+                ?:return@executeTransaction
+            task.content = content
+        }
+    }
+
+    private fun create(content: String){
+        realm.executeTransaction{
+            val task = it.createObject(Task::class.java, UUID.randomUUID().toString())
+            task.content = content
+        }
+    }
+
+    private fun readAll(): RealmResults<Task>{
+        return  realm.where(Task::class.java).findAll().sort("createdAt", Sort.ASCENDING)
+    }
+
+    private fun delete(id :String){
+        realm.executeTransaction{
+            val task = realm.where(Task::class.java).equalTo("id",id).findFirst()
+                ?: return@executeTransaction
+            task.deleteFromRealm()
+        }
+    }
+
+    private fun deleteAll(){
+        realm.executeTransaction {
+            realm.deleteAll()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+
+}
---
+++ app/src/main/java/com/ayushio/urlsummarizer/Task.kt
@@ -0,0 +1,11 @@
+package com.ayushio.urlsummarizer
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Task(
+    @PrimaryKey open var id: String = UUID.randomUUID().toString(),
+    open var content: String = "",
+    open var createdAt: Date = Date(System.currentTimeMillis())
+) : RealmObject()
